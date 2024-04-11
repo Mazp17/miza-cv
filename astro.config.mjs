f808ec9beb82f70729ffe866568e5ebc1cc90351
Miguel Zapata <zpmaepp@gmail.com>
2024-04-11 11:53:39 -0500

    fix: resolve conflicts vercer analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
+  // ...
   output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+  adapter: vercel(),
 });
-// https://astro.build/config
-
